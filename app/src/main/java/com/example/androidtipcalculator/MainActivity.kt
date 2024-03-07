89f7cd1e8c892e8f06bacec31c7de99dd2bb5f4f
CHUAN HE <fantasylink@gmail.com>
2024-03-07 01:04:51 -0800

    done expect styling

--- app/src/main/java/com/example/androidtipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/androidtipcalculator/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.example.androidtipcalculator
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -52,8 +53,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipCalculatorScreen() {
     var serviceCostAmountInput by remember { mutableStateOf("") }
+    var tipPercent by remember { mutableStateOf(15.0) }
     val amount = serviceCostAmountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+
+    val tip = calculateTip(amount,tipPercent)
+    val total = calculateTotal(amount, tipPercent)
     Column(
         modifier = Modifier.padding(32.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -63,14 +67,59 @@ fun TipCalculatorScreen() {
             fontSize = 24.sp,
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
+
+        Button(onClick = {  serviceCostAmountInput= (1..999999).random().toString() })
+        {
+            Text(stringResource(R.string.generate_random_bill))
+            
+        }
+
         Spacer(Modifier.height(16.dp))
         EditServiceCostField(
             value = serviceCostAmountInput,
             onValueChange = { serviceCostAmountInput = it }
         )
+        Column (
+            modifier = Modifier.align(Alignment.CenterHorizontally))
+           {
+            Row {
+                Button(onClick = { tipPercent = 10.0 }, Modifier.padding(top = 8.dp)) {
+                    Text(stringResource(R.string.ten_percent))
+                }
+                Button(onClick = { tipPercent = 15.0 }, Modifier.padding(top = 8.dp)) {
+                    Text(stringResource(R.string.fifteen_percent))
+                }
+            }
+            Row {
+                Button(onClick = { tipPercent = 18.0 }, Modifier.padding(top = 8.dp)) {
+                    Text(stringResource(R.string.eighteen_percent))
+                }
+                Button(onClick = { tipPercent = 20.0 }, Modifier.padding(top = 8.dp)) {
+                    Text(stringResource(R.string.twenty_percent))
+                }
+            }
+        }
+
+        Text("Current Tip: $tipPercent%")
+
+        Spacer(Modifier.height(24.dp))
+        Text(
+            text = stringResource(R.string.bill_amount, amount),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
         Spacer(Modifier.height(24.dp))
         Text(
             text = stringResource(R.string.tip_amount, tip),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(Modifier.height(24.dp))
+
+        Text(
+            text = stringResource(R.string.total, total),
             modifier = Modifier.align(Alignment.CenterHorizontally),
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold
@@ -95,12 +144,21 @@ onValueChange: (String) -> Unit
 
 }
 
+
 private fun calculateTip(
     amount: Double,
-    tipPercent: Double = 15.0
+    tipPercent: Double
 ): String {
     val tip = tipPercent / 100 * amount
     return NumberFormat.getCurrencyInstance().format(tip)
+}
+
+private fun calculateTotal(
+    amount: Double,
+    tipPercent: Double
+): String {
+    val total = tipPercent / 100 * amount+amount
+    return NumberFormat.getCurrencyInstance().format(total)
 }
 @Preview(showBackground = true)
 @Composable
